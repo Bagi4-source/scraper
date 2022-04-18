f720353f269e7299e245bc4b3495d9b1833c96fe
Герман Багдасарян <german@MacBook-Pro-German.local>
2022-04-18 23:05:38 +0300

    commit

--- API/management/commands/request.py
+++ API/management/commands/request.py
@@ -69,7 +69,7 @@ def get_status(code):
         '509': 'Bandwidth Limit Exceeded',
         '510': 'Not Extended'
     }
-    return f"{code}: {code_arr[code]}"
+    return f"{code}: {code_arr[str(code)]}"
 
 
 class Web:
